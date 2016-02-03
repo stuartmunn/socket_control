2fed54b9c5a46a3f7be9bfbf69cf817c32a9d394
stuartmunn <stuart@munn.scot>
2016-02-03 11:44:49 +0000

    Initial version code based entirely on https://pythonhosted.org/energenie/examples/web/. Will be using this as a base to add more sockets and ajax code to reduce page reloads on the client.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import time
+from energenie import switch_on, switch_off
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/on1/')
+def on():
+    switch_on(1)
+    return render_template('index.html')
+   
+@app.route('/off1/')
+def off():
+    switch_off(1)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0') #, port=int("5001"))
+
